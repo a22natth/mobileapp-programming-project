2a6e071eda3b2c48b1f669606bdd53363cdf71aa
a22natth <a22natth@student.his.se>
2023-05-18 12:44:11 +0200

    code added to bind data to viewholder

--- app/src/main/java/com/example/project/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/project/RecyclerViewAdapter.java
@@ -2,11 +2,17 @@ package com.example.project;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     public List<Drinks> items;
     private LayoutInflater layoutInflater;
@@ -14,6 +20,33 @@ public class RecyclerViewAdapter {
     RecyclerViewAdapter(Context context) {
         this.layoutInflater = LayoutInflater.from(context);
         this.items = new ArrayList<>();
+
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater.inflate(R.layout.recyclerview_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(items.get(position).getName()+items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.title);
+        }
 
     }
 
